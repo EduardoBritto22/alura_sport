422a88b1bde4d09d7adb9ed8b7da4caffc5566ef
Eduardo Britto <eduardoobritto@gmail.com>
2022-12-03 21:19:22 +0100

    Add Firebase Authentication dependencies.

--- app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
@@ -17,6 +17,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        setUpNavController()
+    }
+
+    private fun setUpNavController() {
         navController.addOnDestinationChangedListener { _, destination, _ ->
             title = destination.label
             viewModel.components.observe(this) {
