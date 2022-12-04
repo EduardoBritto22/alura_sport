c9723ab8df3591fa8631ad410209d787609fcf2c
Eduardo Britto <eduardoobritto@gmail.com>
2022-12-04 19:11:46 +0100

    Add some custom error messages

--- app/src/main/java/br/com/alura/aluraesporte/repository/FirebaseAuthRepository.kt
+++ app/src/main/java/br/com/alura/aluraesporte/repository/FirebaseAuthRepository.kt
@@ -4,26 +4,24 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.*
 
 private const val TAG = "FirebaseAuthRepository"
 
 class FirebaseAuthRepository(private val firebaseAuth: FirebaseAuth) {
 
-    public fun logoutUser(firebaseAuth: FirebaseAuth) {
+    fun logoutUser(firebaseAuth: FirebaseAuth) {
         firebaseAuth.signOut()
     }
 
-    public fun verifyUser(firebaseAuth: FirebaseAuth) {
+    fun verifyUser(firebaseAuth: FirebaseAuth) {
         val currentUser: FirebaseUser? = firebaseAuth.currentUser
         if (currentUser != null) {
         } else {
         }
     }
 
-    public fun authenticateUser(firebaseAuth: FirebaseAuth) {
+    fun authenticateUser(firebaseAuth: FirebaseAuth) {
         firebaseAuth.signInWithEmailAndPassword(
             "alex@aluraesporte.com",
             "teste123"
@@ -34,7 +32,7 @@ class FirebaseAuthRepository(private val firebaseAuth: FirebaseAuth) {
         }
     }
 
-    public fun registerUser(email: String, password: String): LiveData<Resource<Boolean>> {
+    fun registerUser(email: String, password: String): LiveData<Resource<Boolean>> {
         val liveData = MutableLiveData<Resource<Boolean>>()
         val task: Task<AuthResult> =
             firebaseAuth.createUserWithEmailAndPassword(email, password)
@@ -44,9 +42,15 @@ class FirebaseAuthRepository(private val firebaseAuth: FirebaseAuth) {
             liveData.value = Resource(true)
         }
 
-        task.addOnFailureListener {
-            Log.i(TAG, "registerUser: Failure", it)
-            liveData.value = Resource(false,"Failure to register the user")
+        task.addOnFailureListener {exception->
+            Log.i(TAG, "registerUser: Failure", exception)
+            val errorMessage = when(exception){
+                is FirebaseAuthWeakPasswordException -> "The password must have at least 6 digits"
+                is FirebaseAuthInvalidCredentialsException -> "Invalid email"
+                is FirebaseAuthUserCollisionException -> "Email already registered"
+                else -> "Unknown error"
+            }
+            liveData.value = Resource(false, errorMessage)
         }
 
         return liveData
